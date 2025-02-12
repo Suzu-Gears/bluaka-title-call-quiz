ce2e739679eae5175449038932a7b08da3bb4838
Suzu-Gears <47468734+Suzu-Gears@users.noreply.github.com>
2025-02-12 18:32:22 +0900

    現時点 仮コミット

---
+++ src/lib/getSpreadSheetJSON.ts
@@ -0,0 +1,47 @@
+import { GAS_DEPLOY_ID } from '@/server-constants';
+import fs from 'node:fs';
+import path from 'node:path';
+
+interface SpreadsheetConfig {
+    spreadsheetId: string;
+    sheetName: string;
+    outputDir: string;
+    outputFile: string;
+}
+
+export async function getSpreadSheetJSON(
+    config: SpreadsheetConfig,
+): Promise<any> {
+    const { spreadsheetId, sheetName, outputDir, outputFile } = config;
+    const DOWNLOAD_URL = `https://script.google.com/macros/s/${GAS_DEPLOY_ID}/exec?id=${spreadsheetId}&name=${sheetName}`;
+    const filePath = path.join(outputDir, outputFile);
+
+    if (fs.existsSync(filePath)) {
+        console.log(`File already exists at ${filePath}. Reading existing file.`);
+        const existingData = fs.readFileSync(filePath, "utf-8");
+        return JSON.parse(existingData);
+    }
+
+    try {
+        console.log(`Downloading JSON data from sheet:${sheetName}`);
+        console.log(DOWNLOAD_URL);
+        const response = await fetch(DOWNLOAD_URL);
+        if (!response.ok) {
+            throw new Error(`Failed to fetch data: ${response.statusText}`);
+        }
+
+        const data = await response.json();
+
+        if (!fs.existsSync(outputDir)) {
+            fs.mkdirSync(outputDir, { recursive: true });
+        }
+
+        fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
+
+        console.log(`Data saved to ${filePath}`);
+        return data;
+    } catch (error) {
+        console.error("Error downloading JSON:", error);
+        throw error;
+    }
+}
