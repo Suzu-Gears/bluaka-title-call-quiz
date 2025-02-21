a7a7eee35e1c40d2d5c604baf3604a0bba38d2b2
Suzu-Gears <47468734+Suzu-Gears@users.noreply.github.com>
2025-02-21 15:53:08 +0900

    TypeScriptの単体実行でも環境変数を読み込めるようにした。

--- src/server-constants.ts
+++ src/server-constants.ts
@@ -1,15 +1,19 @@
-export const CUSTOM_DOMAIN =
-  import.meta.env.CUSTOM_DOMAIN || process.env.CUSTOM_DOMAIN || '' // <- Set custom domain if you have. e.g. suzu-mono-gram.com
-export const BASE_PATH =
-  import.meta.env.BASE_PATH || process.env.BASE_PATH || '' // <- Set sub directory path if you want. e.g. /docs/
+const env =
+  typeof import.meta !== 'undefined' && import.meta.env
+    ? import.meta.env
+    : process.env
 
-export const GAS_DEPLOY_ID =
-  import.meta.env.GAS_DEPLOY_ID || process.env.GAS_DEPLOY_ID || ''
-export const SPREADSHEET_ID =
-  import.meta.env.SPREADSHEET_ID || process.env.SPREADSHEET_ID || ''
-export const STUDENTS_SHEET_NAME =
-  import.meta.env.STUDENTS_SHEET_NAME || process.env.STUDENTS_SHEET_NAME || ''
-export const QUIZGEM_VERSION =
-  import.meta.env.QUIZGEM_VERSION || process.env.QUIZGEM_VERSION || ''
+export const CUSTOM_DOMAIN = env.CUSTOM_DOMAIN || '' // <- Set custom domain if you have. e.g. suzu-mono-gram.com
+export const BASE_PATH = env.BASE_PATH || '' // <- Set sub directory path if you want. e.g. /docs/
 
-export const PUBLIC_GA_TRACKING_ID = import.meta.env.PUBLIC_GA_TRACKING_ID || ''
+export const GAS_DEPLOY_ID = env.GAS_DEPLOY_ID || ''
+export const SPREADSHEET_ID = env.SPREADSHEET_ID || ''
+export const STUDENTS_SHEET_NAME = env.STUDENTS_SHEET_NAME || ''
+export const QUIZGEM_VERSION = env.QUIZGEM_VERSION || ''
+
+export const R2_ACCESS_KEY_ID = env.R2_ACCESS_KEY_ID || ''
+export const R2_SECRET_ACCESS_KEY = env.R2_SECRET_ACCESS_KEY || ''
+export const R2_ENDPOINT = env.R2_ENDPOINT || ''
+export const R2_BUCKET_NAME = env.R2_BUCKET_NAME || ''
+
+export const PUBLIC_GA_TRACKING_ID = env.PUBLIC_GA_TRACKING_ID || ''
