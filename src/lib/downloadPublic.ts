f63a4f3b5f941ce9c2aea341f77dd4c25b883754
Suzu-Gears <47468734+Suzu-Gears@users.noreply.github.com>
2025-02-24 17:56:19 +0900

    初期に欠けたファイルもダウンロードさせてしまう

--- src/lib/downloadPublic.ts
+++ src/lib/downloadPublic.ts
@@ -1,4 +1,8 @@
 import { downloadR2Folder } from '@/lib/cloudflareR2Client'
+import {
+  getMissingAudioBySchaledb,
+  getMissingImageBySchaledb,
+} from '@/lib/schaleDBClient'
 
 console.log('Downloading R2 folders...')
 
@@ -7,5 +11,7 @@ await Promise.all([
   downloadR2Folder('image', 'public/image'),
   downloadR2Folder('quiz', 'public'),
 ])
+await getMissingAudioBySchaledb()
+await getMissingImageBySchaledb()
 
 console.log('Download completed for R2 folders')
