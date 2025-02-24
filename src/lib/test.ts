da1c80596ad01be903e3ae7349b75af7652c6b33
Suzu-Gears <47468734+Suzu-Gears@users.noreply.github.com>
2025-02-24 17:38:48 +0900

    削除

--- src/lib/test.ts
+++ src/lib/test.ts
@@ -1,21 +1,15 @@
 import {
   convertToArray,
   getFilteredSchaleDB,
+  getMissingAudioBySchaledb,
+  getMissingImageBySchaledb,
   getSchaleDB,
   type Student,
   type Students,
 } from '@/lib/schaleDBClient'
 
-async function loadAndExtractStudents() {
-  const students_row: Students = await getFilteredSchaleDB()
-  const students: Students = students_row as Students
-  // studentsのkeyofを表示
-  if (students.length > 0) {
-    const studentKeys = Object.keys(students[0]) as (keyof Student)[]
-    console.log('keyof students:', studentKeys)
-  } else {
-    console.log('students array is empty')
-  }
+try {
+  await getMissingAudioBySchaledb()
+} catch (error) {
+  console.error('Error occurred:', error) // エラー処理
 }
-
-loadAndExtractStudents()
