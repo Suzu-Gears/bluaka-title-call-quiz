0424c5c316bbb64223308760b0304996d22f8e2b
Suzu-Gears <47468734+Suzu-Gears@users.noreply.github.com>
2025-02-26 21:06:40 +0900

    test関数更新

--- src/lib/test.ts
+++ src/lib/test.ts
@@ -1,3 +1,14 @@
-import { main } from '@/lib/jsonUtils'
+import { makeQuestionText } from '@/lib/makeQuestionText'
+import { getQuizOptionsJson } from './getSpreadSheetJSON'
+
+async function main() {
+  const quizParams = await getQuizOptionsJson()
+  await Promise.all(
+    quizParams.map(async (quizParam) => {
+      const filePath = await makeQuestionText(quizParam)
+      console.log(`File created at: ${filePath}`)
+    }),
+  )
+}
 
 main()
